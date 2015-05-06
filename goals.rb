99b073539fe74516802921cdfcba87cb21674381
Brent Laverty <brenton.laverty@gmail.com>
2015-05-05 20:17:42 -0400

    Added my name and personal goals to goals.rb

---
+++ goals.rb
@@ -0,0 +1,6 @@
+# Brent Laverty
+
+=begin
+I'm taking this class because I want to better communicate with developers at Vox Media
+in addition to making prototypes grounded in the reailties of our platform.
+=end
